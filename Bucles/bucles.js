52431525932404f96d125ebcb434113f22f463ad
codekren <karen.gallardo.b1@gmail.com>
2023-09-02 15:59:12 -0400

    bucles

--- Bucles/bucles.js
+++ Bucles/bucles.js
@@ -1,10 +1,151 @@
 
-const num= prompt ("Ingrese un num entre 1 y 100")
+// const num= prompt ("Ingrese un num entre 1 y 100")
+// let contador= num
+// for (let i=1; i<=num; i++){   
+//      console.log(contador) 
+//     contador-=1     
+// }
 
-for (let i=1; i<=num; i++){
-    
-     console.log(i)
-}
+// const multiplo=prompt ("ingrese un numero del 1 al 10")
+
+// for (let i=1; i<=10; i++){
+//      console.log(`${i} x ${multiplo} = ${multiplo * i}`)
+// }
+
+// let num=Number(prompt("ingresar numero"))
+// let total=0
+// while (num!=0) {
+//     total= total+num
+//     num=Number(prompt("ingrese otro numero"))
+// }
+// console.log(total)
+
+// let num
+// let total=0
+// do {
+//      num=Number(prompt ("ingrese numero"))
+
+//      total+=num
+     
+// } while (num!=0);
+// console.log(total)
+
+// const total=5
+// let num
+// let i=0
+// do {
+//      i++
+//     num=Number(prompt ("ingrese un numero"))
+//     if(num>total) {
+//      console.log("el num ingresado es mayor")
+//     }
+//     else if (num<total){
+//      console.log("el num ingresado es menor")
+//     }
+//     else {console.log(`acertaste, lo intentaste ${i} veces`) }
+// } while (total!=num);
+
+// let num=Number(prompt("ingrese un numero"))
+
+// for (let i = 1; i <=num ; i++) {
+//      if(num%i==0){
+//           console.log(num/i)
+//      }
+// }
+
+// const colores=["rojo","verde","azul"]
+
+// for (const color of colores) {
+//      console.log(color)
+     
+// }
+
+
+// const numeros=[1,2,3,4,5,]
+
+// for (const num of numeros) {
+//      console.log(`el numero es ${num} y su doble es ${num*2}`)
+     
+// }
+
+// let familia=[{nombre:"Juan",
+//     apellido:"Perez",
+//     edad: 15,
+//     integrante:"Padre"
+//     } , 
+//     {nombre:"Juanita",
+//     apellido:"Perez",
+//     edad: 35,
+//     integrante:"hermano"
+//     } ,
+//     {nombre:"Juanito",
+//     apellido:"Perez",
+//     edad: 25,
+//     integrante:"Tío"
+//     } , 
+//     {nombre:"Juancito",
+//     apellido:"Perez",
+//     edad: 45,
+//     integrante:"Abuelo"
+//     }]
+
+//    for (const miembro of familia) {
+//      console.log(`Hola soy ${miembro.nombre} ${miembro.apellido}(${miembro.integrante}) y tengo ${miembro.edad} años`)
+//      // console.log(miembro)
+     
+//    }
+
+// let persona={nombre:"Juan",
+//     apellido:"Perez",
+//     edad: 15,
+//     integrante:"Padre",
+//     sexo: "masculino"
+// }
+
+// for (const propiedad in persona) {
+//      console.log(propiedad)
+
+// }
+
+// let persona={nombre:"Juan",
+//     apellido:"Perez",
+//     edad: 15,
+//     integrante:"Padre",
+//     sexo: "masculino"
+// }
+
+// for (const propiedad in persona) {
+//      console.log(persona[propiedad])
+
+// }
+
+// let numeros
+// let sumapar=0
+// let sumaimpar=0
+
+// do {
+//      numeros=Number(prompt("ingrese número"))
+//      if(numeros%2 ==0 ){
+//      sumapar+=numeros
+//      }
+//      else { sumaimpar+=numeros}
+       
+     
+// } while (numeros!=0);
+// console.log("Par: " + sumapar)
+
+// console.log("Impar: " + sumaimpar)
+
+// let numeros=[10,21,32,45,5,6,7,8,9,10]
+// let mayor=0
+// for (let i = 0; i < numeros.length; i++) {
+//      if(numeros[i]>mayor){
+//      mayor = numeros[i];
+//      }
+// }
+// console.log(mayor)
+
+
 
 
 
